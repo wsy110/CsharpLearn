e43ada6f136e9c3bb3ad4c90fb04e21e257fa082
Shiyuan Wang <shiyuanwang@microsoft.com>
2023-03-15 17:29:07 +0800

    a

---
+++ ConsoleApp_Test2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp_Test2 {
+    internal class Program {
+        static void Main(string[] args) {
+            var empList = new List<Employee>
+          {
+            new Employee {ID = 1, FName = "John", Age = 23, Sex = 'M'},
+            new Employee {ID = 2, FName = "Mary", Age = 25, Sex = 'F'},
+            new Employee {ID = 3, FName = "Amber", Age = 23, Sex = 'M'},
+            new Employee {ID = 4, FName = "Kathy", Age = 25, Sex = 'F'},
+            new Employee {ID = 5, FName = "Lena", Age = 27, Sex = 'F'},
+            new Employee {ID = 6, FName = "Bill", Age = 28, Sex = 'M'},
+            new Employee {ID = 7, FName = "Celina", Age = 27, Sex = 'F'},
+            new Employee {ID = 8, FName = "John", Age = 28, Sex = 'M'}
+         };
+
+            // query with lamda expression g.Key代表Age和Sex两字段，Count为新增字段<br>        // 最终返回的集合QueryWithLamda包含字段：Age、Sex、Count
+            var QueryWithLamda = empList.GroupBy(x => new { x.Age, x.Sex })
+                        .Select(g => new { g.Key, Count = g.Count() });
+
+            //query with standard expression<br>        //返回结果同上
+            var query = from el in empList
+                        group el by new { el.Age, el.Sex } into g
+                        select new { g.Key, Count = g.Count() };
+
+            /*foreach (var employee in QueryWithLamda *//* Or  QueryWithLamda *//* ) {
+                Console.WriteLine(employee.Count);
+                Console.WriteLine(employee.Key);
+            }*/
+            var QueryWithLamda1 = empList.GroupBy(x => new { x.Age, x.Sex }).ToDictionary(x => x.Key, x => {
+                return x.OrderByDescending(x => x.ID).Take(1).ToList();
+            });
+            foreach (var employee in QueryWithLamda1 /* Or  QueryWithLamda */ ) {
+                Console.WriteLine(employee.Key);
+                Console.WriteLine(employee.Value.First().ID);
+            }
+
+            //{ Age = 23, Sex = M }
+            //            3
+            //{ Age = 25, Sex = F }
+            //            4
+            //{ Age = 27, Sex = F }
+            //            7
+            //{ Age = 28, Sex = M }
+            //            8
+        }
+
+
+    }
+
+    public class Employee {
+        public int ID { get; set; }
+        public string FName { get; set; }
+        public int Age { get; set; }
+        public char Sex { get; set; }
+    }
+}
---
+++ ConsoleApp_TestExtend/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_TestExtend {
+
+
+    internal class Program {
+        static void Main(string[] args) {
+            var parents = new List<Parent>();
+            //Test1(parents);
+            //Test2(parents);
+            //Test3(parents);
+            var children = new List<Child>();
+            Child Child = new Child();
+            Child.Id = 1;
+            Child.Name = "a";
+            children.Add(Child);
+            Test1(children);
+            //Test2(children);
+            Test3(children);
+        }
+
+        private static void Test1<T>(List<T> persons) where T : Parent {
+            foreach (var person in persons) {
+                Console.WriteLine(person.Id);
+
+            }
+        }
+        private static void Test2(List<Parent> persons) {
+        }
+        private static void Test3(IEnumerable<Parent> persons) {
+            foreach (var person in persons) {
+                Console.WriteLine(person.Id);
+
+            }
+
+        }
+    }
+    public class Parent {
+        public int Id { get; set; }
+    }
+
+    public class Child : Parent {
+
+
+        public string Name { get; set; }
+
+    }
+
+
+}
